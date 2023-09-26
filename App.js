f70c196fb971dabf5d750f1c8a39ffae31fec35c
LivinhaR <livianrodrigues980@gmail.com>
2023-09-26 11:26:02 -0300

    Atualização-26/09

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import Routes from "./src/routes";
 export default function App(){
   return(
     <NavigationContainer>
+      <StatusBar backgroundColor="#F0F4FF" barStyle="dark-content"/>
       <Routes />
     </NavigationContainer>
   );
---
+++ src/pages/SignIn/styles.js
@@ -0,0 +1,14 @@
+import { styled } from "styled-components";
+
+export const Background = styled.View`
+    flex: 1;
+    bacgraund-color: #f0f4ff;
+`;
+
+export const Container = styled.KeyboardAvoidingView``;
+
+export const Logo = styled.image``;
+
+export const AreaInput = styled.View``;
+
+export const Input = styled.TextInput``;
